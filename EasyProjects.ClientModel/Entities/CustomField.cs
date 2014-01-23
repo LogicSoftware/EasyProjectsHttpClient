31d5d5f417a272301e4334c6bfa5c2ea0e31a5d6
ilyav <ilyav@localhost>
2014-01-23 15:00:03 +0300

    Availability update custom fields

---
+++ EasyProjects.ClientModel/Entities/CustomField.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyProjects.ClientModel.Entities.CustomFields
+{
+    public class CustomField
+    {
+
+        #region FieldID
+        private int fieldID;
+        /// <summary>
+        /// ID of the field
+        /// </summary>
+        
+        public int FieldID
+        {
+            get { return fieldID; }
+            set { fieldID = value; }
+        }
+        #endregion
+
+        #region FieldType
+
+        /// <summary>
+        /// Type of the field
+        /// </summary>
+        
+        public int FieldType { get; set; }
+
+        #endregion
+
+        #region Alias
+        /// <summary>
+        /// Unique alias for the field.
+        /// </summary>
+        public string Alias { get; set; }
+
+        #endregion
+
+        public int DataType { get; set; }
+
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is restricted.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if this instance is restricted; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsRestricted { get; set; }
+    }
+}
